482cbfb4412ed29e1d149bc1f0a182631c6c3d0a
twinstar26 <35390609+twinstar26@users.noreply.github.com>
2018-12-04 11:31:35 +0530

    Update Program.cs

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -17,13 +17,7 @@ namespace SpringfieldChallenge
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //Application.Run(new Form2());
-            
            
-            
-
-            
-           // changeform(obj1, obj2);
         }
 
        
